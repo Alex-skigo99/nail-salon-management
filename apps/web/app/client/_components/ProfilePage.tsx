b1dd4217b598b2968e73f0ce599f262d25c92277
Alexander BOLSHUKHIN <skigo99@gmail.com>
2026-03-29 15:07:59 +0300

    move input component to inputs folder

--- apps/web/app/client/_components/ProfilePage.tsx
+++ apps/web/app/client/_components/ProfilePage.tsx
@@ -9,7 +9,7 @@ import { Spinner } from "@/components/ui/spinner";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import EditableMultiInputField from "./EditableMultiInputField";
+import EditableMultiInputField from "@/components/inputs/EditableMultiInputField";
 import ChangePasswordDialog from "@/components/modals/ChangePasswordDialog";
 import { AxiosError } from "axios";
 import { cn } from "@/lib/utils";
